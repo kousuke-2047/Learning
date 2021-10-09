9ce0051e78a711d63f766717dc0cd6ec75636515
kousuke-2047 <39142745+kousuke-2047@users.noreply.github.com>
2021-10-09 23:17:23 +0900

    Create D174.java

---
+++ D174.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        final int BASE_NUM = 1500;
+        int line = sc.nextInt();
+        System.out.println(line * BASE_NUM);
+    }
+}
